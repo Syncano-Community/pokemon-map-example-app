a18b649d26091db886965c8af52aa1d17441f19c
Daniel Siwek <danielsiwek95@gmail.com>
2016-07-29 11:40:27 +0200

    [gh-pages] build page

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+  target: 'web',
+  cache: false,
+  entry: {
+    app: [path.resolve(__dirname, './src/App.jsx')]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.json', '.jsx', '.css', '.scss', '.sass', '.svg', '.styl']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    preLoaders: [
+      {test: /\.js(|x)$/, exclude: [/node_modules/]}
+    ],
+    loaders: [
+      {test: /\.js(|x)$/, exclude:[/node_modules/], loader: 'babel', query: {presets: ['react','es2015']}},
+      {test: /\.(|s)css$/, loaders: ['style-loader', 'css?sourceMap', 'sass?sourceMap']}
+    ],
+    loader: 'react-hot-loader'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true
+    })
+  ]
 };
