148eddbd63fb67622ceb2628b274e6ac5ce393b2
Daniel Siwek <danielsiwek95@gmail.com>
2016-07-29 14:01:30 +0200

    [gh-pages] Build app, fix path, change package.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,31 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  target: 'web',
-  cache: false,
-  entry: {
-    app: [path.resolve(__dirname, './src/App.jsx')]
-  },
+  devtool: 'eval',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js'
-  },
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.js', '.json', '.jsx', '.css', '.scss', '.sass', '.svg', '.styl']
-  },
-  module: {
-    preLoaders: [
-      {test: /\.js(|x)$/, exclude: [/node_modules/]}
-    ],
-    loaders: [
-      {test: /\.js(|x)$/, exclude:[/node_modules/], loader: 'babel', query: {presets: ['react','es2015']}},
-      {test: /\.(|s)css$/, loaders: ['style-loader', 'css?sourceMap', 'sass?sourceMap']}
-    ],
-    loader: 'react-hot-loader'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: './'
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
       inject: true
     })
-  ]
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+    { test: /\.jsx?$/, loaders: ['babel'], include: path.join(__dirname, 'src') }
+    ]
+  }
 };
