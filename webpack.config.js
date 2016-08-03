31fb7b596990a52d16f9417a89782d62c5d4d09c
Daniel Siwek <danielsiwek95@gmail.com>
2016-08-03 15:46:28 +0200

    Improve package.json and web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'eval',
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: './'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
@@ -26,5 +24,11 @@ module.exports = {
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true
+    })
+  ]
 };
